a625c2ce284ef006e37364bc1c6383cbc8a5cc8c
hosam <hosamelmalt@gmail.com>
2020-12-16 09:32:57 +0200

    Initial Commit

---
+++ Resizable App Bar/Resizable_App_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resizable_App_BarApp.swift
+//  Resizable App Bar
+//
+//  Created by hosam on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct Resizable_App_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
